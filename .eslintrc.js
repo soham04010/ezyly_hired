4fb740b814a75eff379fc2dd05ba6d3e0cdb4d06
Soham Chaudhary <chaudharisoham041@gmail.com>
2025-06-06 22:59:34 +0530

    done

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  rules: {
+    "react/no-unescaped-entities": "off",
+    "@next/next/no-img-element": "off",
+    "@typescript-eslint/no-unused-vars": "off",
+    "react/jsx-no-comment-textnodes": "off",
+  },
+};
